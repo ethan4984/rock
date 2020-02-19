ff612686baad95097439749251e3ae3282fdc958
ethan <55000224+ethan4984@users.noreply.github.com>
2020-02-19 01:39:35 -0700

    Update cross.sh

--- Tools/cross.sh
+++ Tools/cross.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-echo c,c++ or c, or c++?
-read lang
-
-cores=grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}'
-
 curl -O https://ftp.gnu.org/gnu/gcc/gcc-9.2.0/gcc-9.2.0.tar.xz
 curl -O https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.xz
 
@@ -21,25 +16,15 @@ mkdir ../Bin
 
 cd build_binutils
 ../binutils-2.34/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
-make -j$(cores)
-make install -j$(cores)
-
-if[-z"$(which -- $TARGET-as)"] then
-	echo fatal: Error in building binutils
-	exit 1
-fi
+make 
+make install 
 
 cd ../build-gcc
 ../gcc-9.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=$(lang) --without-headers
-make all-gcc -j$(cores)
-make all-target=libgcc -j$(cores)
-make install-gcc -j$(cores)
-make install-target-libgcc -j$(cores)
-
-if[-e"$($HOME/opt/cross/build-gcc/i686-elf-gcc)" ] then
-	echo fatal: Error in building gcc
-	exit 1
-fi
+make all-gcc 
+make all-target=libgcc -
+make install-gcc 
+make install-target-libgcc 
 
 
 
