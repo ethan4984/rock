a1cc02a7200ea9efa8d82a0cb58b6997ca2ee916
Ethan <ethanm4984@gmail.com>
2024-10-24 09:29:28 -0600

    skeleton for io servers

--- servers/fs/vfs/fayt/debug.h
+++
@@ -1,7 +0,0 @@
-#ifndef FAYT_DEBUG_H_
-#define FAYT_DEBUG_H_
-
-void print(const char *str, ...);
-void panic(const char *str, ...);
-
-#endif
--- servers/io/nvme/fayt/compiler.h
+++
@@ -1,7 +0,0 @@
-#ifndef FAYT_COMPILER_H_
-#define FAYT_COMPILER_H_
-
-#define unlikely(x) __builtin_expect(!!(x), 0)
-#define likely(x) __builtin_expect(!!(x), 1)
-
-#endif
